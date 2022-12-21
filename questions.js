02ebd3f6dcc373c193cf57bc59d17119b8bdf195
wgarv100 <78126184+wgarv100@users.noreply.github.com>
2022-12-21 11:47:47 -0500

    First commit

---
+++ questions.js
@@ -0,0 +1,342 @@
+export const questions = [
+  {
+    question: "In 1768, Captain James Cook set out to explore which ocean?",
+    answers: [
+      { text: "Pacific Ocean", correct: true },
+      { text: "Atlantic Ocean", correct: false },
+      { text: "Guadalupe Mountains", correct: false },
+      { text: "Grand Teton", correct: false },
+    ],
+  },
+
+  {
+    question: "What is actually electricity?",
+    answers: [
+      { text: "A flow of water", correct: false },
+      { text: "A flow of air", correct: false },
+      { text: "A flow of electrons", correct: true },
+      { text: "A flow of atom", correct: false },
+    ],
+  },
+
+  {
+    question: "Which of the following is not an international organisation?",
+    answers: [
+      { text: "FIFA", correct: false },
+      { text: "NATO", correct: false },
+      { text: "ASEAN", correct: false },
+      { text: "FBI", correct: true },
+    ],
+  },
+
+  {
+    question: "Which of the following disorders is the fear of being alone?",
+    answers: [
+      { text: "Agoraphobia", correct: true },
+      { text: "Aerophobia", correct: false },
+      { text: "Acrophobia", correct: false },
+      { text: "Arachnophobia", correct: false },
+    ],
+  },
+
+  {
+    question: "Which song is by the German heavy metal band “Scorpions”?",
+    answers: [
+      { text: "Stairway to Heaven", correct: false },
+      { text: "Wind of Change", correct: true },
+      { text: "Don't Stop Me Now", correct: false },
+      { text: "Hey Jude", correct: false },
+    ],
+  },
+
+  {
+    question: "What is the speed of sound?",
+    answers: [
+      { text: "120 km/h", correct: false },
+      { text: "1,200 km/h", correct: true },
+      { text: "400 km/h", correct: false },
+      { text: "700 km/h", correct: false },
+    ],
+  },
+
+  {
+    question: "Which is the easiest way to tell the age of many trees?",
+    answers: [
+      { text: "Measure the width", correct: false },
+      { text: "Count the rings", correct: true },
+      { text: "Count the leaves", correct: false },
+      { text: "Measure the height", correct: false },
+    ],
+  },
+
+  {
+    question: "What do we call a newly hatched butterfly?",
+    answers: [
+      { text: "A moth", correct: false },
+      { text: "A butterfly", correct: false },
+      { text: "A caterpillar", correct: true },
+      { text: "A chrysalis", correct: false },
+    ],
+  },
+
+  {
+    question: "In total, how many novels were written by the Bronte sisters?",
+    answers: [
+      { text: "4", correct: false },
+      { text: "5", correct: false },
+      { text: "6", correct: false },
+      { text: "7", correct: true },
+    ],
+  },
+
+  {
+    question: "Which did Viking people use as money?",
+    answers: [
+      { text: "Rune stones", correct: false },
+      { text: "Jewellery", correct: true },
+      { text: "Seal skins", correct: false },
+      { text: "Wool", correct: false },
+    ],
+  },
+
+  {
+    question:
+      "What was the first country to use tanks in combat during World War I?",
+    answers: [
+      { text: "France", correct: false },
+      { text: "Japan", correct: false },
+      { text: "Britain", correct: true },
+      { text: "Germany", correct: false },
+    ],
+  },
+
+  {
+    question: "What is the main component of the sun?",
+    answers: [
+      { text: "Liquid lava", correct: false },
+      { text: "Gas", correct: true },
+      { text: "Molten iron", correct: false },
+      { text: "Rock", correct: false },
+    ],
+  },
+
+  {
+    question: "Goulash is a type of beef soup in which country?",
+    answers: [
+      { text: "Hungary", correct: true },
+      { text: "Czech Republic", correct: false },
+      { text: "Slovakia", correct: false },
+      { text: "Ireland", correct: false },
+    ],
+  },
+
+  {
+    question: "Which two months are named after Emperors of the Roman Empire?",
+    answers: [
+      { text: "January and February", correct: false },
+      { text: "March and April", correct: false },
+      { text: "May and June", correct: false },
+      { text: "July and August", correct: true },
+    ],
+  },
+
+  {
+    question: "Which of the following animals can run the fastest?",
+    answers: [
+      { text: "Cheetah", correct: true },
+      { text: "Leopard", correct: false },
+      { text: "Tiger", correct: false },
+      { text: "Lion", correct: false },
+    ],
+  },
+
+  {
+    question: "Where did the powers of Spiderman come from?",
+    answers: [
+      { text: "Born with them", correct: false },
+      { text: "Spider bite", correct: true },
+      { text: "Scientific experiment", correct: false },
+      { text: "Woke up after a dream", correct: false },
+    ],
+  },
+
+  {
+    question:
+      "What is the most points that a player can score with a single throw in darts?",
+    answers: [
+      { text: "20", correct: false },
+      { text: "40", correct: false },
+      { text: "60", correct: true },
+      { text: "80", correct: false },
+    ],
+  },
+
+  {
+    question: "What is American football called in the United Kingdom?",
+    answers: [
+      { text: "Rugby", correct: false },
+      { text: "American football", correct: true },
+      { text: "Handball", correct: false },
+      { text: "Combball", correct: false },
+    ],
+  },
+
+  {
+    question:
+      "Which of the following actors was the first one to play James Bond?",
+    answers: [
+      { text: "Timothy Dalton", correct: false },
+      { text: "Roger Moore", correct: false },
+      { text: "Sean Connery", correct: true },
+      { text: "George Lazenby", correct: false },
+    ],
+  },
+
+  {
+    question: "Which of the following songs was the top-selling hit in 2019?",
+    answers: [
+      { text: "Someone You Loved", correct: true },
+      { text: "Old Town Road", correct: false },
+      { text: "I Don't Care", correct: false },
+      { text: "Bad Guy", correct: false },
+    ],
+  },
+  {
+    question: "In which country is Transylvania?",
+    answers: [
+      { text: "Bulgaria", correct: false },
+      { text: "Romania", correct: true },
+      { text: "Croatia", correct: false },
+      { text: "Serbia", correct: false },
+    ],
+  },
+  {
+    question: "Which football club does Jordan Henderson play for?",
+    answers: [
+      { text: "Liverpool", correct: true },
+      { text: "Manchester City", correct: false },
+      { text: "Tottenham", correct: false },
+      { text: "Chelsea", correct: false },
+    ],
+  },
+  {
+    question: "The two biggest exporters of beers in Europe are Germany and",
+    answers: [
+      { text: "Spain", correct: false },
+      { text: "France", correct: false },
+      { text: "Italy", correct: false },
+      { text: "Belgium", correct: true },
+    ],
+  },
+  {
+    question: '"I think, therefore I am" was coined by which philosopher?',
+    answers: [
+      { text: "Socrates", correct: false },
+      { text: "Plato", correct: false },
+      { text: "Aristotle", correct: false },
+      { text: "Descartes", correct: true },
+    ],
+  },
+  {
+    question:
+      "In “Game of Thrones”, Winterfell is the ancestral home of which family?",
+    answers: [
+      { text: "The Lannisters", correct: false },
+      { text: "The Starks", correct: true },
+      { text: "The Tully's", correct: false },
+      { text: "The Targaryens", correct: false },
+    ],
+  },
+  {
+    question: "Who is known as the Patron Saint of Spain?",
+    answers: [
+      { text: "St Patrick", correct: false },
+      { text: "St Benedict", correct: false },
+      { text: "St James", correct: true },
+      { text: "St John", correct: false },
+    ],
+  },
+  {
+    question: "What does the term “SOS” commonly stand for?",
+    answers: [
+      { text: "Save Our Sheep", correct: false },
+      { text: "Save Our Ship", correct: false },
+      { text: "Save Our Seal", correct: false },
+      { text: "Save Our Soul", correct: true },
+    ],
+  },
+  {
+    question: "Which company is known for publishing the Mario video game?",
+    answers: [
+      { text: "Xbox", correct: false },
+      { text: "Nintendo", correct: true },
+      { text: "SEGA", correct: false },
+      { text: "Electronic Arts", correct: false },
+    ],
+  },
+  {
+    question:
+      "We often use sodium bicarbonate in the kitchen. What is its other name?",
+    answers: [
+      { text: "Vinegar", correct: false },
+      { text: "Sugar", correct: false },
+      { text: "Salt", correct: false },
+      { text: "Baking Soda", correct: true },
+    ],
+  },
+  {
+    question: "Which was the first film by Disney to be produced in color?",
+    answers: [
+      { text: "Toy Story", correct: false },
+      { text: "Sleeping Beauty", correct: false },
+      { text: "Snow White", correct: true },
+      { text: "Cinderella", correct: false },
+    ],
+  },
+  {
+    question:
+      "What is the name of the first book of the Old Testament in the Bible?",
+    answers: [
+      { text: "Exodus", correct: false },
+      { text: "Genesis", correct: true },
+      { text: "Proverbs", correct: false },
+      { text: "Matthew", correct: false },
+    ],
+  },
+  {
+    question: "Who was the second person to step on the moon?",
+    answers: [
+      { text: "Yuri Gagarin", correct: false },
+      { text: "James Irwin", correct: false },
+      { text: "Alan Bean", correct: false },
+      { text: "Buzz Aldrin", correct: true },
+    ],
+  },
+  {
+    question: "How many time zones are there in total in the world?",
+    answers: [
+      { text: "8", correct: false },
+      { text: "16", correct: false },
+      { text: "24", correct: true },
+      { text: "32", correct: false },
+    ],
+  },
+  {
+    question: "What is the rarest type of blood in the human body?",
+    answers: [
+      { text: "AB negative", correct: true },
+      { text: "O positive", correct: false },
+      { text: "B negative", correct: false },
+      { text: "A positive", correct: false },
+    ],
+  },
+  {
+    question: "Cu is the chemical symbol for which element?",
+    answers: [
+      { text: "Oxygen", correct: false },
+      { text: "Copper", correct: true },
+      { text: "Zinc", correct: false },
+      { text: "Helium", correct: false },
+    ],
+  },
+];
